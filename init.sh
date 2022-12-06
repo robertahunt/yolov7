f74a07f3c6c797f67ee9ec7b39cec3421f2969cf
Roberta Hunt <rehunt@ualberta.ca>
2022-12-06 12:16:17 +0100

    modifications

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#export PATH=$PATH:/home/rob/.local/bin
+#python3 -m pip install black pytest gitpython
+#git clone -n git@github.com:robertahunt/phd_00_autoencoding_species.git $REPO_FOLDER
+#cd ${REPO_FOLDER}
+#git checkout $GIT_COMMIT
+#cd $CWD
+bash
